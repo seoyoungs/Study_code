c52f17fea27fd237206bf56f649428047dbd93d2
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-09-23 08:27:28 +0900

    Add files via upload

---
+++ Custom_Object/helmet/helmet_keggle.py
@@ -0,0 +1,84 @@
+# https://www.kaggle.com/bouweceunen/hardhat-detection-with-tensorflow/?select=train
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from PIL import Image
+from matplotlib import pyplot as plt
+from tensorflow.python.util import compat
+from tensorflow.core.protobuf import saved_model_pb2
+from google.protobuf import text_format
+import pprint
+import os
+
+# ! rm -rf ./models && git clone https://github.com/tensorflow/models.git \
+#     && cd models/research \
+#     && protoc object_detection/protos/*.proto --python_out=. \
+#     && cp object_detection/packages/tf2/setup.py . && \
+#     python3 -m pip install --use-feature=2020-resolver .
+
+from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import dataset_util, label_map_util
+from object_detection.protos import string_int_label_map_pb2
+
+# reconstruct frozen graph
+def reconstruct(pb_path):
+    if not os.path.isfile(pb_path):
+        print("Error: %s not found" % pb_path)
+
+    print("Reconstructing Tensorflow model")
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.compat.v1.GraphDef()
+        with tf.io.gfile.GFile(pb_path, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+    print("Success!")
+    return detection_graph
+
+# visualize detection
+def image2np(image):
+    (w, h) = image.size
+    return np.array(image.getdata()).reshape((h, w, 3)).astype(np.uint8)
+
+def image2tensor(image):
+    npim = image2np(image)
+    return np.expand_dims(npim, axis=0)
+
+# %matplotlib inline
+def detect(detection_graph, test_image_path):
+    with detection_graph.as_default():
+        gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.01)
+        with tf.compat.v1.Session(graph=detection_graph,config=tf.compat.v1.ConfigProto(gpu_options=gpu_options)) as sess:
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+            image = Image.open(test_image_path)
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: image2tensor(image)}
+            )
+
+            npim = image2np(image)
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                npim,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                # category_index,
+                use_normalized_coordinates=True,
+                line_thickness=5)
+            plt.figure(figsize=(12, 8))
+            plt.imshow(npim)
+            plt.show()
+
+
+
+
+
+
+
