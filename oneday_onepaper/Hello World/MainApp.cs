1a49e859b3aed6d932f8749ac1819cc904212fa7
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-11-17 08:53:34 +0900

    Add files via upload

---
+++ oneday_onepaper/Hello World/MainApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello_World
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?" + "\n" + "반갑습니다!");
+
+            //string s = "여러분, 안녕하세요?반갑습니다!";
+            //s = s.Replace("?", "? \n");
+            //Console.WriteLine(s);
+        }
+    }
+}
