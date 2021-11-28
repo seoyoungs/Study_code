9f3b2a63f3ec993e47e6c81e6ace052a6f42d9cd
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-11-29 08:37:17 +0900

    Add files via upload

---
+++ oneday_onepaper/DoWhile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            do
+            {
+                Console.WriteLine("a) i :{0}", i--);
+            }
+            while (i > 0);
+
+            do
+            {
+                //0이지만 여기서도 한차례 실행
+                Console.WriteLine("b) i :{0}", i--);
+            }
+            while (i > 0);
+        }
+    }
+}
