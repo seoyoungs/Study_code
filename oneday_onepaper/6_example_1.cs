56d573453a09146d0b5f18cab57ec96928bd95fa
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-11-30 08:41:56 +0900

    Add files via upload

---
+++ oneday_onepaper/6_example_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_example_1
+{
+    class Program
+    {
+        static double Square(double arg)
+        {
+            double result;
+            result = arg * arg;
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요: ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+        }
+    }
+}
