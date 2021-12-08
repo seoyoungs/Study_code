93a84894b0a32f6369de08d2d843dd01197cfec6
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-12-08 11:21:42 +0900

    Add files via upload

---
+++ oneday_onepaper/Queue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> que = new Queue<int>(); // queue를 저장할 정수 배열 생성
+            //Queue que = new Queue();
+            
+            que.Enqueue(1);
+            que.Enqueue(2);
+            que.Enqueue(3);
+            que.Enqueue(4);
+            que.Enqueue(5);
+
+            Console.WriteLine("que.Count: {0}", que.Count); //queue에 저장된 개수
+            Console.WriteLine();
+
+            while (que.Count > 0)
+                Console.WriteLine(que.Dequeue());
+            Console.WriteLine();
+
+            Console.WriteLine("que.Count: {0}", que.Count);
+        }
+    }
+}
