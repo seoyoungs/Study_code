634cb153655bd454a235f247ebbe947db435400a
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-12-28 10:29:47 +0900

    Add files via upload

---
+++ oneday_onepaper/SealedMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SealedMethod
+{
+    class Base
+    {
+        public virtual void SealMe()
+        {
+
+        }
+    }
+    class Derived : Base
+    {
+        public sealed override void SealMe()
+        {
+        }
+    }
+    class WantToOverride : Derived
+    {
+        public override void SealMe() // 본인 되어 override  할 수 없어서 에러가 난다.
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
